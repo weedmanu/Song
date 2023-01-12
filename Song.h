575a6214afc379eb33086ad42a8df991cf24b5b6
Weedmanu <terraspi13@gmail.com>
2023-01-12 10:35:36 +0100

    Add files via upload

---
+++ Song.h
@@ -0,0 +1,24 @@
+#ifndef SONG_H
+#define SONG_H
+
+void beep(int Buzzer, int note, int duree);     // les fonctions du cpp
+
+void Alerte(int piezzo);
+void Starwars(int piezzo); 
+void GameOfThrones(int piezzo); 
+void HappyBirthday(int piezzo); 
+void HarryPotter(int piezzo); 
+void Pirate(int piezzo); 
+void mario(int piezzo); 
+void McGyver(int piezzo); 
+void StarWars2(int piezzo); 
+void BonBruteTruand(int piezzo); 
+void IndianaJones(int piezzo); 
+void twentyCentFox(int piezzo); 
+void looney(int piezzo); 
+void Entertainement(int piezzo); 
+void BarbieGirl(int piezzo); 
+void Greensleaves(int piezzo); 
+void Bond(int piezzo); 
+
+#endif
